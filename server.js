c1387f6b6a94a52d6983c1955d07ca72f721261f
Kasper Falk <Kasper@Ditnavns-MacBook-Pro.local>
2019-09-17 11:23:12 +0200

    server working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hej verden din luder')
 })
  
 app.listen(3000)
